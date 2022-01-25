7b0644bdd71934d55964f044feeb2bdf4ea19432
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2022-01-26 11:53:18 +1300

    delete dotnet code

--- RabbitMQProxy/EnvironmentConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RabbitMQProxy;
-
-public static class EnvironmentConfiguration
-{
-    public static string BrokerHostName => Environment.GetEnvironmentVariable("BROKER_HOSTNAME") ?? "localhost";
-    public static int BrokerPort => Environment.GetEnvironmentVariable("BROKER_PORT") != null ? int.Parse(Environment.GetEnvironmentVariable("RABBITMQ_PORT")) : 5672;
-    public static string BrokerUserName => Environment.GetEnvironmentVariable("BROKER_USERNAME") ?? "guest";
-    public static string BrokerPassword => Environment.GetEnvironmentVariable("BROKER_PASSWORD") ?? "guest";
-}
